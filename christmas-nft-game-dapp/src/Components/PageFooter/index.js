ff09bdaf600462f1451d863b5e068d27e894b761
Coder Wally <97439397+coderwally@users.noreply.github.com>
2022-11-23 17:41:25 +0100

    layout changes

--- christmas-nft-game-dapp/src/Components/PageFooter/index.js
+++ christmas-nft-game-dapp/src/Components/PageFooter/index.js
@@ -1,19 +1,21 @@
 import twitterLogo from "../../assets/twitter-logo.svg";
 import "./PageFooter.css";
-const TWITTER_HANDLE = "_buildspace";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE_ME = "traderwally7";
+const AUTHOR = "coderwally (a.k.a. @traderwally7)";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE_ME}`;
 
 const PageFooter = () => {
   return (
-    <>
+    <div className="footer-container">
       <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
       <a
-        className="footer-text"
+        className="footer-link"
         href={TWITTER_LINK}
         target="_blank"
         rel="noreferrer"
-      >{`built with @${TWITTER_HANDLE}`}</a>
-    </>
+      >{`Project for buildspace.so - developer: ${AUTHOR}`}</a>
+      <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+    </div>
   );
 };
 
